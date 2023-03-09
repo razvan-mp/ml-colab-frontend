5397ab67a76fb700b36c9d5d21341b58f43a06e7
Răzvan Morcov-Pahoncea <razvan10190@gmail.com>
2023-03-09 19:54:10 +0200

    fix knn and add support for K selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front-end';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
 
   static hidePlotly(): void {
     setTimeout(() => {
